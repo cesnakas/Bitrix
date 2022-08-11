dd0ad51fb8964837e64390ed1233f78095c3843e
Sergei ★ Česnakas <10352420+cesnakas@users.noreply.github.com>
2022-08-11 21:42:45 +0400

    Update header

--- local/templates/main/header.php
+++ local/templates/main/header.php
@@ -1,16 +1,24 @@
 <?php
-if (!defined('B_PROLOG_INCLUDED') || B_PROLOG_INCLUDED !== true) die();
+use Bitrix\Main\Engine\CurrentUser;
 use Bitrix\Main\Page\Asset;
+use Bitrix\Main\UI\Extension;
+if (!defined('B_PROLOG_INCLUDED') || B_PROLOG_INCLUDED !== true) die();
 ?>
 <!DOCTYPE html>
-<html lang="<?=LANGUAGE_ID;?>">
+<html lang="<?=LANGUAGE_ID?>">
 <head>
 
     <?php
 	$APPLICATION->ShowHead();
+    // Meta tags & etc
 	Asset::getInstance()->addString('<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no, viewport-fit=cover, user-scalable=0">');
-	Asset::getInstance()->addCss(SITE_TEMPLATE_PATH.'/css/main.css');
-	Asset::getInstance()->addJs(SITE_TEMPLATE_PATH.'/js/main.js');
+    // Bootstrap
+    Extension::load('ui.bootstrap4');
+    // Custom CSS / JS
+    Asset::getInstance()->addCss(SITE_TEMPLATE_PATH . '/css/main.css');
+    Asset::getInstance()->addJs(SITE_TEMPLATE_PATH . '/js/main.js');
+    // Analytics
+    include_once $_SERVER['DOCUMENT_ROOT'] . '/local/include/analytics.php';
     ?>
 
     <title><?php $APPLICATION->ShowTitle()?></title>
@@ -20,9 +28,7 @@ use Bitrix\Main\Page\Asset;
 </head>
 <body>
 
-    <?php
-    $APPLICATION->ShowPanel()
-    ?>
+    <?= (CurrentUser::get()->isAdmin()) ? $APPLICATION->ShowPanel() : null ?>
 
     <header>
 
