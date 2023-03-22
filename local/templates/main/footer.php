bc41474c7f4942a7743bc711254ec5fb2d8149e9
Sergei ★ Česnakas <10352420+cesnakas@users.noreply.github.com>
2023-03-22 12:34:39 +0400

    Update template

--- local/templates/main/footer.php
+++ local/templates/main/footer.php
@@ -2,11 +2,11 @@
 if (!defined('B_PROLOG_INCLUDED') || B_PROLOG_INCLUDED !== true) die();
 ?>
 
-	</main>
+  </main>
 
-	<footer role="contentinfo">
+  <footer role="contentinfo">
 
-	</footer>
+  </footer>
 
 </body>
 </html>
--- local/templates/main/header.php
+++ local/templates/main/header.php
@@ -5,33 +5,33 @@ use Bitrix\Main\UI\Extension;
 if (!defined('B_PROLOG_INCLUDED') || B_PROLOG_INCLUDED !== true) die();
 ?>
 <!DOCTYPE html>
-<html lang="<?=LANGUAGE_ID?>">
+<html lang="<?= LANGUAGE_ID ?>">
 <head>
 
-    <?php
-	$APPLICATION->ShowHead();
-    // Meta tags & etc
-	Asset::getInstance()->addString('<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no, viewport-fit=cover, user-scalable=0">');
-    // Bootstrap
-    Extension::load('ui.bootstrap4');
-    // Custom CSS / JS
-    Asset::getInstance()->addCss(SITE_TEMPLATE_PATH . '/css/main.css');
-    Asset::getInstance()->addJs(SITE_TEMPLATE_PATH . '/js/main.js');
-    // Analytics
-    include_once $_SERVER['DOCUMENT_ROOT'] . '/local/include/analytics.php';
-    ?>
+  <?php
+  $APPLICATION->ShowHead();
+  // Meta tags & etc
+  Asset::getInstance()->addString('<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no, viewport-fit=cover, user-scalable=0">');
+  // Bootstrap
+  Extension::load('ui.bootstrap4');
+  // Custom CSS / JS
+  Asset::getInstance()->addCss(SITE_TEMPLATE_PATH . '/css/main.css');
+  Asset::getInstance()->addJs(SITE_TEMPLATE_PATH . '/js/main.js');
+  // Analytics
+  include_once $_SERVER['DOCUMENT_ROOT'] . '/local/include/analytics.php';
+  ?>
 
-    <title><?php $APPLICATION->ShowTitle()?></title>
+  <title><?php $APPLICATION->ShowTitle() ?></title>
 
-    <link rel="shortcut icon" type="image/x-icon" href="/favicon.ico">
+  <link rel="shortcut icon" type="image/x-icon" href="/favicon.ico">
 
 </head>
 <body>
 
-    <?= (CurrentUser::get()->isAdmin()) ? $APPLICATION->ShowPanel() : null ?>
+  <?= (CurrentUser::get()->isAdmin()) ? $APPLICATION->ShowPanel() : null ?>
 
-    <header>
+  <header role="banner">
 
-    </header>
+  </header>
 
-    <main role="main">
+  <main role="main">
