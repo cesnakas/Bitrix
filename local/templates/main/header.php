48f9dfa61dde979ee479de24f95d0f8c56b3d54d
Sergei Česnakas <10352420+cesnakas@users.noreply.github.com>
2023-05-03 19:22:19 +0400

    Update files

--- local/templates/main/header.php
+++ local/templates/main/header.php
@@ -1,8 +1,8 @@
 <?php
+if (!defined('B_PROLOG_INCLUDED') || B_PROLOG_INCLUDED !== true) die();
 use Bitrix\Main\Engine\CurrentUser;
 use Bitrix\Main\Page\Asset;
 use Bitrix\Main\UI\Extension;
-if (!defined('B_PROLOG_INCLUDED') || B_PROLOG_INCLUDED !== true) die();
 ?>
 <!DOCTYPE html>
 <html lang="<?= LANGUAGE_ID ?>">
